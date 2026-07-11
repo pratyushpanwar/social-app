8b85738e7255882920be4496e624cc4476aaab32
Pratyush Panwar <avipanwar1212@gmail.com>
2026-07-11 14:12:32 +0530

    proxy change

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': "https://api-source-eegm.onrender.com"
+    }
+  },
   plugins: [react(), tailwindcss()],
 })
